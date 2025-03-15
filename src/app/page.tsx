df0ff8e0cb2ff65c4750563b5d0e220f919131ed
Akashkr28 <email.akash2010@gmail.com>
2025-03-15 15:36:49 +0530

    Added User Information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import {Button} from "@/components/ui/button"
-import {useAuthActions} from "@convex-dev/auth/react"; 
+import { UserButton } from "@/features/auth/components/user-button";
+
 export default function Home() {
-  const {signOut} = useAuthActions();
 
   return (
     <div>
       <h1>Home</h1>
-      <Button onClick={() => signOut()}>
-        Sign Out
-      </Button>
+      <UserButton />
     </div>
   );
 }
