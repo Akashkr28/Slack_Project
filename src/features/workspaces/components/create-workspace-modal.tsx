2601d42cd41633c7c2544d9c5b3c519bedbf7f08
Akashkr28 <email.akash2010@gmail.com>
2025-03-30 22:34:16 +0530

    Workspace creation continues

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -3,26 +3,59 @@
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogHeader,
     DialogTitle,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const CreateWorkspaceModal = () => {
+    const router = useRouter();
     const [open, setOpen] = useCreateWorkspaceModal();
+    const [name, setName] = useState("");
+
+    const { mutate, isPending } = useCreateWorkspace();
 
     const handleClose = () => {
         setOpen(false);
         //TODO: reset form
     }
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        mutate({name}, {
+            onSuccess(data) {
+                console.log(data);
+            },
+        })
+    }
+ 
     return (
         <Dialog open={open} onOpenChange={handleClose}>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Create workspace</DialogTitle>
                 </DialogHeader>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <Input
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        disabled={isPending}
+                        required
+                        autoFocus
+                        minLength={3}
+                        placeholder="Workspace name e.g. 'My workspace'"
+                    />
+                <div className="flex justify-end">
+                    <Button disabled={isPending}>Create</Button>
+                </div>
+                </form>
+
             </DialogContent>
         </Dialog>
     )
